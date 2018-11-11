1656d30a1df0bf5363c1dd96830e557927b37cf4
Danny Seo <dannyseo517@gmail.com>
2018-11-10 17:13:16 -0800

    add database support

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ app.get('/', function (req, res) {
 var server = app.listen(8080, function() {
     console.log('listening on port 8080...');
 });
+
+
+var mysql = require('mysql');
+const conf = require('./config/conf.json');
+var connection = mysql.createConnection(conf.db);
+
+try {
+    connection.connect();
+    var sql = "SELECT * FROM users";
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+    });
+
+    connection.end();
+} catch (e) {
+    console.log(e);
+    connection.end();
+}
