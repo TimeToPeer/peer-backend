407b55d2cb77149584ab849658f7c3372eaf22ed
Danny Seo <dannyseo517@gmail.com>
2018-11-22 03:44:09 -0800

    password

--- database/random.js
+++ database/random.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const { db } = require('../config/conf.json');
-
+const mainHelper = require('../helper/main-helper');
 const connection = mysql.createConnection(db);
 
 connection.connect((err) => {
@@ -13,36 +13,37 @@ connection.connect((err) => {
         console.log(err1, result);
     });
 
+    mainHelper.createSaltHashPassword('password').then((hashedPassword) => {
+        for (let i = 0; i < 1; i += 1) {
+            sql = `INSERT INTO users (username, password, type, class_code, school_code, personality) 
+                    VALUES ('test.teacher1', '${hashedPassword}', '1', '1000', '1000', 'test')`;
+            
+            connection.query(sql, (err2, result) => {
+                if (err2) throw err2;
+                console.log(err2, result);
+            });
 
-    for (let i = 0; i < 1; i += 1) {
-        sql = `INSERT INTO users (username, password, type, class_code, school_code, personality) 
-                VALUES ('test.teacher1', 'password', '1', '1000', '1000', 'test')`;
+            sql = `INSERT INTO users (username, password, type, class_code, school_code, personality) 
+                    VALUES ('test.teacher1', '${hashedPassword}', '1', '1001', '1000', 'test')`;
 
-        connection.query(sql, (err2, result) => {
-            if (err2) throw err2;
-            console.log(err2, result);
-        });
+            connection.query(sql, (err3, result) => {
+                if (err3) throw err3;
+                console.log(err3, result);
+            });
+        }
 
-        sql = `INSERT INTO users (username, password, type, class_code, school_code, personality) 
-                VALUES ('test.teacher1', 'password', '1', '1001', '1000', 'test')`;
+        for (let j = 0; j < 60; j += 1) {
+            const name = `test.student${j}`;
+            const classCode = j < 30 ? '1000' : '1001';
+            sql = `INSERT INTO users (username, password, type, class_code, school_code, personality) 
+                    VALUES ('${name}', '${hashedPassword}', '2', '${classCode}', '1000', 'test')`;
 
-        connection.query(sql, (err3, result) => {
-            if (err3) throw err3;
-            console.log(err3, result);
-        });
-    }
+            connection.query(sql, (err4, result) => {
+                if (err4) throw err4;
+                console.log(err4, result);
+            });
+        }
 
-    for (let j = 0; j < 60; j += 1) {
-        const name = `test.student${j}`;
-        const classCode = j < 30 ? '1000' : '1001';
-        sql = `INSERT INTO users (username, password, type, class_code, school_code, personality) 
-                VALUES ('${name}', 'password', '2', '${classCode}', '1000', 'test')`;
-
-        connection.query(sql, (err4, result) => {
-            if (err4) throw err4;
-            console.log(err4, result);
-        });
-    }
-
-    connection.end();
+        connection.end();
+    });
 });
